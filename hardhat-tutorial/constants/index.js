f692442f33aed2bf8a1d0d8e7a7475a4cffd894e
Joseph Anthony Braganza <josephanthonybraganza@Josephs-MacBook-Air.local>
2023-06-06 19:17:23 +0530

    deployed the code

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xe89b22bCE4EcDdE6d5538213e4692098eb8CAf33";
+const WHITELIST_CONTRACT_ADDRESS = "0xbCa23bCc57ceF4C3dE79ea638FE3d4d76cF205D9";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-enp3ajm6x-joseph2001-braganza.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
